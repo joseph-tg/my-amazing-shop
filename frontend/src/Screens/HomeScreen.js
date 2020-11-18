ae75749c9e8daaef2ef0fca32138b577d30f0a51
joseph-tg <josephgatimu111@gmail.com>
2020-11-18 19:55:12 +0300

    Added final frontend files

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,25 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data';
 
 function HomeScreen(props) {
     return <ul className="products">
-    {
-      data.products.map(product =>
+      {
+        data.products.map(product => 
           <li>
             <div className="product">
-                <img className="product-image" src={product.image} alt="product" />
-                <div className="product-name">
-                    <a href="product.html">{product.name}</a>
-                </div>
-                <div className="product-brand">{product.brand}</div>
-                <div className="product-price">${product.price}</div>
-                <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
+            <Link to={'/products/' + product._id}>
+              <img className="product-image" src={product.image} alt="product"></img>
+              </Link>
+              
+              <div className="product-name">
+                <Link to={'/products/' + product._id}>{product.name}</Link>
+              </div>
+              <div className="product-brand">{product.brand}</div>
+              <div className="product-price">${product.price}</div>
+              <div className="product-rating">{product.rating} Stars ({product.numReviews} Reviews)</div>
             </div>
           </li>
         )
-    }
+      }
     
   </ul>
 }
 
 export default HomeScreen;
+
